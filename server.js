c7f6114733ec1f09ddb2929691305ce255600ab4
Matioma <Artiom.Matievschi@gmail.com>
2021-01-29 11:13:30 +0100

    Add default project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(__dirname + "/dist/web-game-engine"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(_dirname) + "/dist/web-game-engine/index.html");
+});
+
+app.listen(process.env.PORT || 8080);

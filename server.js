8b2d3f5d4dd718b9453bf66193ab1461ebeb2865
Matioma <Artiom.Matievschi@gmail.com>
2021-01-29 12:22:43 +0100

    setup heroku deply

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require("path");
 const app = express();
 
 console.log("Weird");
-app.use(express.static(__dirname + "/dist/webgameengine"));
+app.use(express.static(__dirname + "/dist/WebGameEngineClient"));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/webgameengine/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/WebGameEngineClient/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);

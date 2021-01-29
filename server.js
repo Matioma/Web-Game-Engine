c558e3f46e5c50cec48aa6e1dae77c9437480c5e
Matioma <Artiom.Matievschi@gmail.com>
2021-01-29 11:21:26 +0100

    Fix brakets

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/web-game-engine"));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(_dirname) + "/dist/web-game-engine/index.html");
+  res.sendFile(path.join(__dirname + "/dist/web-game-engine/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);

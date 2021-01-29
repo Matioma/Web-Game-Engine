ebf1786ef471c7bf065e36205e21f53fed66c887
Matioma <Artiom.Matievschi@gmail.com>
2021-01-29 11:29:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+console.log("Weird");
 app.use(express.static(__dirname + "/dist/web-game-engine"));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/dist/web-game-engine/index.html"));
+  res.send("COOl");
+  //   res.sendFile(path.join(__dirname + "/dist/web-game-engine/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
